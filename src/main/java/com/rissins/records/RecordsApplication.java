e551dddcdffa47fe6a98e9a3ae63ba52734686ee
rissins <tnalsqqq@naver.com>
2022-02-14 23:10:14 +0900

    Webhook test

--- src/main/java/com/rissins/records/RecordsApplication.java
+++ src/main/java/com/rissins/records/RecordsApplication.java
@@ -2,7 +2,7 @@ package com.rissins.records;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//test
 @SpringBootApplication
 public class RecordsApplication {
 
