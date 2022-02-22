f3f5a61504fc17fc6456137975e8e1c21ce22a4c
rissins <tnalsqqq@naver.com>
2022-02-23 03:01:47 +0900

    refactor: LocalDateTime KST 설정

--- src/main/java/com/rissins/records/RecordsApplication.java
+++ src/main/java/com/rissins/records/RecordsApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @EnableJpaAuditing
 @SpringBootApplication
 public class RecordsApplication {
@@ -12,4 +15,8 @@ public class RecordsApplication {
 		SpringApplication.run(RecordsApplication.class, args);
 	}
 
+	@PostConstruct
+	void started() {
+		TimeZone.setDefault(TimeZone.getTimeZone("KST"));
+	}
 }
