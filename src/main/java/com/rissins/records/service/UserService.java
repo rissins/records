36dda9e2d86b07be3a4156f3c33eebc984ace338
rissins <tnalsqqq@naver.com>
2022-02-23 03:30:08 +0900

    refactor: 로그인 필터링 방식 변경

--- src/main/java/com/rissins/records/service/UserService.java
+++ src/main/java/com/rissins/records/service/UserService.java
@@ -23,16 +23,23 @@ public class UserService {
         return userRepository.findAll();
     }
 
+//    public int login(User user) {
+//        Optional<User> first = userRepository.findAll().stream()
+//                .filter(m -> Objects.equals(m.getUserId(), user.getUserId()))
+//                .findFirst();
+//        User checkUser = first.filter(m -> Objects.equals(m.getUserPassword(), user.getUserPassword()))
+//                .orElse(null);
+//        if (checkUser == null) {
+//            return 1;
+//        } else {
+//            return 0;
+//        }
+//    }
     public int login(User user) {
-        Optional<User> first = userRepository.findAll().stream()
-                .filter(m -> Objects.equals(m.getUserId(), user.getUserId()))
-                .findFirst();
-        User checkUser = first.filter(m -> Objects.equals(m.getUserPassword(), user.getUserPassword()))
-                .orElse(null);
-        if (checkUser == null) {
-            return 1;
-        } else {
-            return 0;
-        }
+        return findByUserId(user.getUserId()).getUserPassword().equals(user.getUserPassword()) ? 0 : 1;
+    }
+
+    public User findByUserId(String userId) {
+        return userRepository.findByUserId(userId);
     }
 }
