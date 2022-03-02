348a9aee883fb47f2a925b88426ddcb8db89161c
rissins <tnalsqqq@naver.com>
2022-03-02 15:00:36 +0900

    refactor: Err_incomplete_chunked_encoding 수정 테스트

--- src/main/resources/static/js/planAjax.js
+++ src/main/resources/static/js/planAjax.js
@@ -1,42 +1,42 @@
-function planSave() {
-    var param = {
-        'title': $("#planTitle").val(),
-        'context': $("#planContext").val(),
-        'userId': $("#planUserId").val(),
-    }
-
-    $.ajax({
-        url: '/plan',
-        type: 'POST',
-        data: param,
-    }).done(function () {
-        alert("계획등록 완료");
-        window.location.reload();
-    }).fail(function () {
-            alert("실패");
-        }
-    );
-}
-
-function viewPlanData(userId) {
-
-    $.ajax({
-        url: "/api/v1/plan?userId=" + userId,
-        type: "GET",
-        dataType: "json",
-    }).done(function (planData) {
-        console.log(planData);
-        // document.getElementById('planViewTitle').innerText = data[0].title;
-        // document.getElementById('planViewContext').innerText = data[0].context;
-        var htmlOut='';
-        for(var idx in planData){
-            htmlOut += '<tr>';
-            htmlOut += '    <td>'+(planData[idx].title)+'</td>';
-            htmlOut += '    <td>'+(planData[idx].context)+'</td>';
-            htmlOut += '</tr>';
-        }
-        $('#planViewModalBody').append(htmlOut);
-    }).fail(function () {
-        console.log("실패");
-    });
-}
+// function planSave() {
+//     var param = {
+//         'title': $("#planTitle").val(),
+//         'context': $("#planContext").val(),
+//         'userId': $("#planUserId").val(),
+//     }
+//
+//     $.ajax({
+//         url: '/plan',
+//         type: 'POST',
+//         data: param,
+//     }).done(function () {
+//         alert("계획등록 완료");
+//         window.location.reload();
+//     }).fail(function () {
+//             alert("실패");
+//         }
+//     );
+// }
+//
+// function viewPlanData(userId) {
+//
+//     $.ajax({
+//         url: "/api/v1/plan?userId=" + userId,
+//         type: "GET",
+//         dataType: "json",
+//     }).done(function (planData) {
+//         console.log(planData);
+//         // document.getElementById('planViewTitle').innerText = data[0].title;
+//         // document.getElementById('planViewContext').innerText = data[0].context;
+//         var htmlOut='';
+//         for(var idx in planData){
+//             htmlOut += '<tr>';
+//             htmlOut += '    <td>'+(planData[idx].title)+'</td>';
+//             htmlOut += '    <td>'+(planData[idx].context)+'</td>';
+//             htmlOut += '</tr>';
+//         }
+//         $('#planViewModalBody').append(htmlOut);
+//     }).fail(function () {
+//         console.log("실패");
+//     });
+// }
