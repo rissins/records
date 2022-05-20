e72ab5b468ed4395d185fd8e2d1778804d60e3e1
rissins <tnalsqqq@naver.com>
2022-05-20 22:13:51 +0900

    test: 계획삭제 테스트작성

--- src/test/java/com/rissins/records/service/PlanServiceTest.java
+++ src/test/java/com/rissins/records/service/PlanServiceTest.java
@@ -3,21 +3,27 @@ package com.rissins.records.service;
 import com.rissins.records.domain.Plan;
 import com.rissins.records.dto.PlanResponse;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
+@TestMethodOrder(value = MethodOrderer.OrderAnnotation.class)
 class PlanServiceTest {
 
     @Autowired
     PlanService planService;
 
     @Test
+    @Order(1)
     void 계획_추가() {
         //given
         String title = "계획제목테스트";
@@ -34,5 +40,21 @@ class PlanServiceTest {
         //then
         Assertions.assertThat(allByUserId.get(0).getTitle()).isEqualTo(title);
     }
+
+    @Test
+    @Order(2)
+    void 계획_삭제() {
+        //given
+        String userId = "testId";
+        Long id = planService.findAllByUserId(userId).get(0).getId();
+        List<Long> ids = new ArrayList<>();
+        ids.add(id);
+        //when
+        planService.deleteByIds(ids);
+        //then
+        List<Plan> planFindByUserId = planService.findAllByUserId(userId);
+        Assertions.assertThat(planFindByUserId).isEmpty();
+    }
+
 
 }
